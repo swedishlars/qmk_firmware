2033a23462e16ebcfdf39c987857dda3af140c45
swedishlars <swedishlars@gmail.com>
2021-11-16 15:55:22 +0000

    Added my own rules.mk. Disabled VIA, enabled mouse keys.

---
+++ keyboards/gmmk/pro/iso/keymaps/swedishlars/rules.mk
@@ -0,0 +1,13 @@
+
+# Enable VIA NOTE: VIA breaks key mapping and compiler. Leave Off.
+VIA_ENABLE = no
+
+# NEW Disable mousekey to reduce firmware size
+MOUSEKEY_ENABLE = yes
+
+# Enable tap dance
+# TAP_DANCE_ENABLE = yes
+
+# Enables Link Time Optimization (LTO) when compiling the keyboard
+# TODO qmk warns that LTO likely fails on this board
+# LTO_ENABLE = yes
