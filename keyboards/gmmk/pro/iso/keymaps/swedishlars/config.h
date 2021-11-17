43313598e0a0af853e4b534f35acaad7984dfec3
swedishlars <swedishlars@gmail.com>
2021-11-17 21:20:48 +0000

    Added custom tapdance actions.

--- keyboards/gmmk/pro/iso/keymaps/swedishlars/config.h
+++ keyboards/gmmk/pro/iso/keymaps/swedishlars/config.h
@@ -58,14 +58,14 @@
 #define MOUSEKEY_WHEEL_MAX_SPEED 8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 250
 
-/* // Custom tapdance advanced action. Pass keycode in user_data */
-/* #define ACTION_TAP_DANCE_FN_KEY(user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset, kc) { \ */
-/*     .fn = {user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset}, \ */
-/*     .user_data = (void *)&((qk_tap_dance_pair_t) { kc, 0 }) \ */
-/* } */
+// Custom tapdance advanced action. Pass keycode in user_data
+#define ACTION_TAP_DANCE_FN_KEY(user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset, kc) { \
+    .fn = {user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset}, \
+    .user_data = (void *)&((qk_tap_dance_pair_t) { kc, 0 }) \
+}
 
-/* // Custom tapdance advanced action. Pass keycode and layer in user_data. */
-/* #define ACTION_TAP_DANCE_FN_KEY_LAYER(user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset, kc, layer) { \ */
-/*     .fn = {user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset}, \ */
-/*     .user_data = (void *)&((qk_tap_dance_dual_role_t) {kc, layer, NULL}) \ */
-/* } */
+// Custom tapdance advanced action. Pass keycode and layer in user_data.
+#define ACTION_TAP_DANCE_FN_KEY_LAYER(user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset, kc, layer) { \
+    .fn = {user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset}, \
+    .user_data = (void *)&((qk_tap_dance_dual_role_t) {kc, layer, NULL}) \
+}
